e19c17d4ac5e5d37564f490a5068bacdd102896a
Jeonghyeon <dalpaeng2@gmail.com>
2023-01-04 18:06:18 +0900

    Return 문 파싱

--- parser/parser.go
+++ parser/parser.go
@@ -49,6 +49,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -66,6 +68,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: 세미콜론을 만날 때까지 표현식을 건너뛴다.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
 
 	// TODO: 세미콜론을 만날 때까지 표현식을 건너뛴다.
 	for !p.curTokenIs(token.SEMICOLON) {
